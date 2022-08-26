d65c0570cc18a2e4bc25ce72982ba9200c6b74b8
Bolatito Shobanke <bolatito.shobanke@Terra-045.local>
2022-08-26 13:50:03 +0100

    update signin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middleware/validate-request';
+import { Password } from '../services/password';
+import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -14,7 +18,32 @@ router.post('api/users/signin',
       .notEmpty()
       .withMessage('You must supply a password')
   ], validateRequest, async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
+    const existingUser = await User.findOne({email});
+
+    if(!existingUser) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+
+    if(!passwordsMatch) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    //generate jwt
+    const userJwt = jwt.sign({
+      id: existingUser.id, 
+      email: existingUser.email
+    }, process.env.JWT_KEY!);
+  
+    //set jwt on session object
+    req.session = {
+      jwt: userJwt
+    };
+  
+    res.status(200).send(existingUser);
  
 });
 
