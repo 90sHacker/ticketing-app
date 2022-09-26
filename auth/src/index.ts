7f88bd4d5f87b421d95cd5a918620dc3e40b9fda
Bolatito Shobanke <bolatito.shobanke@Terra-045.local>
2022-09-26 13:30:31 +0100

    update mongodb env for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,12 @@ const start = async () => {
     if(!process.env.JWT_KEY) {
       throw new Error('jwt key not defined');
     }
-    
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+
+    if(!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI must be defined');
+    }
+
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('connected to db');
 
   } catch(e) {
