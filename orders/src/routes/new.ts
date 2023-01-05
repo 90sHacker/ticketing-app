c903058475a56dbe93aef19ed9c77ff483af39f6
Bolatito Shobanke <bolatito.shobanke@Terra-045.local>
2023-01-05 04:50:49 +0100

    [orders] implements OCC for orders and tickets

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -49,6 +49,7 @@ router.post('/api/orders', requireAuth, [
     status: order.status,
     userId: order.userId,
     expiredAt: order.expiresAt.toISOString(),
+    version: order.version,
     ticket: {
       id: ticket.id,
       price: ticket.price
