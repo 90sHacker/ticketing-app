f2b0852b8f002ce3a6f367c6baf0473b8e1c2aa0
Bolatito Shobanke <bolatito.shobanke@Terra-045.local>
2023-03-05 01:12:20 +0100

    [client] fix typo  in signin

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import useRequest from '../../hooks/use-request';
 import Router from "next/router";
 
-const signup = () => {
+const signin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { doRequest, errors} = useRequest({
@@ -41,4 +41,4 @@ const signup = () => {
   </form>)
 }
 
-export default signup
+export default signin
