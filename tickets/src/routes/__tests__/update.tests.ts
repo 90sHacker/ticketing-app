8302a67843726c07a57d2e790f7616af5227c36d
Bolatito Shobanke <bolatito.shobanke@Terra-045.local>
2023-03-06 23:13:50 +0100

    [tickets] test rejecting updates on a reserved ticket

--- tickets/src/routes/__tests__/update.tests.ts
+++ tickets/src/routes/__tests__/update.tests.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 import { natsWrapper } from '../../nats-wrapper';
+import { Ticket } from '../../models/ticket';
 
 it('returns a 404 if the provided ticket id does not exist', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -141,4 +142,30 @@ it('should publish an event', async () => {
     .expect(200)
 
     expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('should reject an update for a reserved ticket', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'Rolling Loud 2022',
+      price: 480
+    })
+    .expect(201)
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket?.set({orderId: new mongoose.Types.ObjectId().toHexString()});
+  await ticket?.save();
+  
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'Rolling Loud 2023',
+      price: 1000
+    })
+    .expect(400)
 })
