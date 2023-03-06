6421af1ec39127dcd72279ff079f3a79bb25322e
Bolatito Shobanke <bolatito.shobanke@Terra-045.local>
2023-03-06 21:27:03 +0100

    publish a ticket-updated event after reserving a ticket

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,6 +3,7 @@ import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
 import { NotFoundError } from "@ticketszone/common";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -17,7 +18,19 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     //set the orderId on the ticket to reserve it, and save the ticket
     ticket.set({orderId: data.id});
     await ticket.save();
+
+    //publish an event
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      version: ticket.version,
+      orderId: ticket.orderId
+    })
+
     //ack the message
     msg.ack();
+
   }
 }
